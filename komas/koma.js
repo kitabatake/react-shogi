b40e50fddbdb0c407847297ab2fa31cc67c01bab
Tatsuya Kitabatake <e2tod2@gmail.com>
2016-08-01 17:04:17 +0900

    implements Koma fundamentals.

--- komas/koma.js
+++ komas/koma.js
@@ -3,20 +3,86 @@ var next_id = 0
 
 class Koma {
 
+  static reset() {
+    next_id = 0
+  }
+
   static getId() {
     return next_id++
   }
 
   constructor(options = {
-    position: null
+    position: null,
+    owner: ''
   }) {
     this.id = Koma.getId()
     this.name = 'hoge'
     this.position = options.position
+    this.previousPosition = null
+    this.owner = options.owner
+    this.nareru = true
+    this.narigoma = false
+  }
+
+  move(x, y) {
+    this.previousPosition = Object.assign({}, this.position)
+    this.position = {x:x, y:y}
+  }
+
+  canNareru() {
+    if (this.narigoma || !this.nareru) return false
+
+    var targetPosition = this.normalizePosition(this.position)
+    if (targetPosition.y < 3) return true
+
+    // narukaeru
+    targetPosition = this.normalizePosition(this.previousPosition)
+    if (targetPosition.y < 3) return true
+
+    return false
+  }
+
+  // Normalize position to sente's point of view. 
+  normalizePosition(position) {
+    if (this.owner == 'sente') return Object.assign({}, position)
+    return {
+      x: position.x,
+      y: 8 - position.y
+    }
+  }
+
+  naru() {
+    this.narigoma = true
   }
 
   render() {
     return this.name
+  }
+
+  getMovement() {
+    var movement = this.narigoma? this.narigomaMovement : this.movement
+    return this.normalizeMovement(movement)
+  }
+
+  normalizeMovement(movement) {
+    if (this.owner == 'sente') return Object.assign({}, movement)
+    return {
+      num:movement.num,
+      dx: movement.dx,
+      dy: movement.dy.map(y => -y)
+    }
+  }
+
+  getMovablePositions() {
+    var positions = []
+    var movement = this.getMovement()
+    for (var i = 0; i < movement.num; i++) {
+      positions.push({
+        x: this.position.x + movement.dx[i], 
+        y: this.position.y + movement.dy[i]})
+    }
+
+    return positions
   }
 }
 
---
+++ test/komas/koma.js
@@ -0,0 +1,125 @@
+import expect from 'expect.js'
+import Koma from '../../komas/koma.js'
+
+const reset = () => {
+  Koma.reset()
+}
+
+const createKoma = (x = 0, y = 0, owner = 'sente') => {
+  var koma = new Koma({
+    position: {x, y},
+    owner: owner
+  })
+  koma.narigomaMovement = {}
+  return koma
+}
+
+describe('Koma#constructor', () => {
+  it('sets positions and owner', () => {
+    var koma = createKoma(1, 2, 'sente')
+    expect(koma.position.x).to.be(1)
+    expect(koma.position.y).to.be(2)
+    expect(koma.owner).to.be('sente')
+  })
+})
+
+describe('Koma#move', () => {
+  it('should be set spefified position', () => {
+    reset()
+    var koma = createKoma(1, 2, 'sente')
+    koma.move(3, 5)
+    expect(koma.position.x).to.be(3)
+    expect(koma.position.y).to.be(5)
+  })
+})
+
+describe('Koma#canNareru', () => {
+  beforeEach(() => {
+    reset()
+  })
+
+  it('is narenai situations', () => {
+    var koma = createKoma(5, 5, 'sente')
+    koma.move(5, 4)
+    expect(koma.canNareru()).to.be(false)
+    koma.move(5, 3)
+    expect(koma.canNareru()).to.be(false)
+
+    koma = createKoma(5, 4, 'gote')
+    koma.move(5, 5)
+    expect(koma.canNareru()).to.be(false)
+
+    // if koma already naru, canNareru() return false
+    koma.naru()
+    koma.move(5, 8)
+    expect(koma.canNareru()).to.be(false)
+  })
+
+  it('is nareru situations', () => {
+    var koma = createKoma(5, 3, 'sente')
+    koma.move(5, 2)
+    expect(koma.canNareru()).to.be(true)
+
+    koma = createKoma(5, 2, 'sente')
+    koma.move(4, 8)
+    expect(koma.canNareru()).to.be(true)
+
+    koma = createKoma(5, 3, 'gote')
+    koma.move(3, 6)
+    expect(koma.canNareru()).to.be(true)
+
+  })
+})
+
+describe('Koma#getMovablePositions', () => {
+  it('simulates hu movable positions', () => {
+    var koma = createKoma(5, 3, 'sente')
+    koma.movement = {
+      num: 1,
+      dx: [0],
+      dy: [-1]
+    }
+
+    var mps = koma.getMovablePositions()
+    expect(mps.length).to.be(1)
+    expect(mps[0].x).to.be(5)
+    expect(mps[0].y).to.be(2)
+  })
+
+  it('is gote movable positions', () => {
+    var koma = createKoma(5, 3, 'gote')
+    koma.movement = {
+      num: 1,
+      dx: [0],
+      dy: [-1]
+    }
+
+    var mps = koma.getMovablePositions()
+    expect(mps.length).to.be(1)
+    expect(mps[0].x).to.be(5)
+    expect(mps[0].y).to.be(4)
+  })
+
+  it('is narigoma movable positions', () => {
+    var koma = createKoma(5, 3, 'sente')
+    koma.movement = {
+      num: 1,
+      dx: [0],
+      dy: [-1]
+    }
+    koma.narigomaMovement = {
+      num: 2,
+      dx: [0, 1],
+      dy: [-1, 1]
+    }
+
+    koma.naru()
+
+    var mps = koma.getMovablePositions()
+    expect(mps.length).to.be(2)
+    expect(mps[0].x).to.be(5)
+    expect(mps[0].y).to.be(2)
+    expect(mps[1].x).to.be(6)
+    expect(mps[1].y).to.be(4)
+  })
+})
